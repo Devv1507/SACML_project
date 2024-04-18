fc1f982528e9f299cf4ddb7bec7165c40505a378
David Valencia <davide.valencia.v@gmail.com>
2024-04-18 01:49:35 -0500

    Some fixes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const app = require('./app.js');
 
 
-const PORT = 3000; // define the port to use
+const PORT = process.env.PORT || 3000; // define the port to use
 
 // an asyncronic function to test for errors during connection
 async function main() {
