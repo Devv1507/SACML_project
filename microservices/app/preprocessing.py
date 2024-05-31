a1d9098d5a263b402da7fe216928a4da36d5e033
David Valencia <davide.valencia.v@gmail.com>
2024-05-31 05:20:18 -0500

    Adding lime graphic for explainability of each prediction

--- microservices/app/preprocessing.py
+++ microservices/app/preprocessing.py
@@ -3,9 +3,17 @@ import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder
 import joblib
+import lime
+import lime.lime_tabular
+import matplotlib.pyplot as plt
+from io import BytesIO
+
+TRAIN_DATA_PATH = 'datasets/cleaned/trainX.csv'
+trainX = pd.read_csv(TRAIN_DATA_PATH)
 
 # Global variables to store the original categories
 original_categories = {}
+
 
 def load_model(model_path):
     return joblib.load(model_path)
@@ -46,8 +54,13 @@ def decode_data(data):
 def preprocess_and_predict(model, data):
     # Prepare the data
     data = prepare_data(data)
-    X = data.drop(columns=['risk'])
-    
+    # Check if 'risk' column exists and drop it
+    if 'risk' not in data.columns:
+        data['risk'] = ""  # Placeholder value (adjust as needed)
+        X = data.drop(columns=['risk'])
+    else:
+        X = data.drop(columns=['risk'])
+
     # Make predictions
     data['predictions'] = model.predict(X)
     data['prediction_probs'] = model.predict_proba(X)[:, 1]
@@ -56,4 +69,23 @@ def preprocess_and_predict(model, data):
     risk_mapping = {0: "bad", 1: "good"}
     data['predictions'] = data['predictions'].map(risk_mapping)
 
-    return data
+ # Generate LIME explanation for the first instance
+    num_col = X.shape[1]
+    print('num_col:', num_col)
+    explainer = lime.lime_tabular.LimeTabularExplainer(training_data=trainX.values,
+                                                           feature_names=trainX.columns,
+                                                           class_names=['bad', 'good'],
+                                                           mode='classification')
+    explained = explainer.explain_instance(X.iloc[0], model.predict_proba, num_features=num_col)
+    fig = explained.as_pyplot_figure()
+
+    # Adjust the figure size and margins
+    fig.set_size_inches(10, 6)
+    plt.tight_layout(rect=[0, 0.1, 1, 1])  # Adjust the bottom margin to fit the labels
+    
+    output = BytesIO()
+    fig.savefig(output, format='png')
+    plt.close(fig)
+    output.seek(0)
+
+    return data, output
--- microservices/app/routes.py
+++ microservices/app/routes.py
@@ -1,28 +1,65 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, send_file
 import pandas as pd
 import joblib
 from .analysis import prepare_data, make_predictions
 from .preprocessing import preprocess_and_predict, load_model
+import json
+import lime
+import lime.lime_tabular
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 main = Blueprint('main', __name__)
 
 MODEL_PATH = 'models/lgbm_model_pipeline.pkl'
 model = load_model(MODEL_PATH)
 
+
+
 @main.route('/predict', methods=['POST'])
 def predict():
-    file = request.files['file']
-    if file:
+    if request.files:
+        file = request.files['file']
+        if file:
+            try:
+                # Check content type to decide how to process the file
+                if 'csv' in file.filename or file.content_type == 'text/csv':
+                    data = pd.read_csv(file)
+                elif 'xls' in file.filename or file.content_type in [ 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
+                    data = pd.read_excel(file)
+                else:
+                    return "Unsupported file type", 400
+            except Exception as e:
+                print(e)
+                return jsonify({'error': str(e)}), 400
+    elif request.is_json:
         try:
-            data = pd.read_csv(file)
-            predictions = preprocess_and_predict(model, data)
-            response = predictions.to_dict(orient='records')
-            return jsonify(response)
+            # If the request body is JSON
+            data_dict = request.get_json()
+            data = pd.DataFrame([data_dict])  # Convert single JSON object to DataFrame
         except Exception as e:
             print(e)
             return jsonify({'error': str(e)}), 400
     else:
-        return jsonify({'error': 'No file provided'}), 400 
+        return jsonify({'error': 'No valid file or JSON provided'}), 400
+
+    # Process the data and make predictions
+    try:
+        predictions, output = preprocess_and_predict(model, data)
+        data_predict = predictions.to_dict(orient='records')
+
+        #send it as a base64 string within JSON
+        img_base64 = base64.b64encode(output.getvalue()).decode('utf-8')
+        response = {
+            'predictions': data_predict,
+            'explanation_image': img_base64
+        }
+        #return send_file(output, mimetype='image/png')
+        return jsonify(response)
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 400
     
 
 
